ed55da080e69971bc030d2dd0c78ee7d1a3d7754
LDEMON1020 <byeongyoon061020@gmail.com>
2025-10-26 03:58:03 +0900

    [feat] 코어 만듦

---
+++ Assets/Core.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Core : MonoBehaviour
+{
+    public GameObject core;
+    public GameObject EnemySpwaner;
+
+    public int maxHP = 100;
+    public int CurrentHP;
+
+    public Slider hpSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHP = maxHP;
+        hpSlider.value = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHP -= damage;
+        hpSlider.value = (float)CurrentHP / maxHP;
+        if (CurrentHP <= 0)
+        {
+            CoreDestroy();
+        }
+    }
+
+    void CoreDestroy()
+    {
+        Destroy(gameObject);
+        Destroy(EnemySpwaner);
+    }
+}
